663eea24a5a7036640d06c81024547c4d7a11867
deepakgahlot <deepakgahlot543@gmail.com>
2015-05-04 14:13:43 +0530

    chat

---
+++ chat.js
@@ -0,0 +1,14 @@
+﻿var express = require("express");
+app = express();
+server = require('http').createServer(app);
+io = require('socket.io').listen(server);
+ server.listen(3000);
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
+});
+io.sockets.on('connection', function (socket) {
+    socket.on('send message', function (data) {
+        io.sockets.emit('new meaasge', data);
+
+    });
+});
